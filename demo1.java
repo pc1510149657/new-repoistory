6e4b3a27c7e5fa0ebf069c6a43d579dd2f72b470
pc1510149657 <54013341+pc1510149657@users.noreply.github.com>
2019-08-12 14:29:17 +0800

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,77 @@
+package com.icss;
+
+import java.util.Scanner;
+
+public class demo1 {
+	public static void main(String[] args) {
+/*		int a = 10;
+		System.out.println(a);//10
+		a++;
+		System.out.println(a);//11
+*///		++a;
+//		System.out.println(a);
+/*		int b = 5 ;
+		System.out.println(b);//5
+		//当多个运算同时出现时，++在变量前，先加1再取值，在变量后先取值再加1
+		b = a++;
+		System.out.println(b);//11
+		System.out.println(a);//12
+		
+		System.out.println(++b);//12
+		System.out.println(b--);//12
+		System.out.println(b);//11
+*/		
+//		int a = 5 ;
+//		a=a+8;
+//		
+//		a += 8;
+//		System.out.println(a);
+//		
+//		byte b =9;
+//		//b=(byte)(b+a);可以自动强制类型转换
+//		b+=a;
+//		System.out.println(b);
+		
+		
+//		System.out.println(3>2);
+//		System.out.println(3>=2);
+//		System.out.println(3<2);
+//		System.out.println(3<=2);
+//		System.out.println(3==2);
+//		System.out.println(3!=2);
+		
+		
+		
+//		System.out.println(true|| true);//true
+//		System.out.println(true|| false);//true
+//		System.out.println(false|| false);//false
+//		System.out.println(false|| true);//true
+//		
+//		System.out.println(!(2>1));//false
+		
+		
+//		System.out.println(2&1);
+		
+		//? :
+		//求两个数中 的最大数
+//		int a = 10;
+//		int b = 13;
+//		int c = 8;
+//		int max = a >b?a:b;
+//		max = max >c ? max:c;
+//		System.out.println("最大值为"+max);
+//		
+		//输入一个数判断这个数是否是偶数
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
